b07bc3496d222cf0c0e8d026cef75247b3f6e57c
Danrlei Martins <danrlei.martins@hotmail.com>
2022-04-27 17:46:23 +0100

    axios and crypto API function added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
 const electron = require('electron');
 const { ipcRenderer } = electron;
+const axios = require('axios'); // HTTP library that connects to APIs
+
+// BTC EUR Section
+var priceEUR = document.getElementById('priceEUR');
+var targetPriceEUR = document.getElementById('targetPriceEUR');
+var targetPriceValEUR;
+
+// Function that will fetch the current BTC price from an API, display it to screen and compare to target price
+function getBTCEUR() {
+    axios.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC&tsyms=EUR')
+        .then(res => {
+            const crypto = res.data.BTC.EUR;
+            priceEUR.innerHTML = '€' + crypto.toLocaleString('en');
+
+        })
+}
+// Run getBTC function and set interval to update price every 10 seconds
+getBTCEUR();
+setInterval(getBTCEUR, 10000);
 
 // Clicking on the button will trigger the ipcRenderer to send a message to ipcMain to open new window
 document.getElementById('notifyBtnEUR').addEventListener('click', event => {
     ipcRenderer.send('main:add');
 });
+
